f4b67c2ec76bb7738514cb7f540a022102f2f178
Victor <victor.du.77@hotmail.fr>
2020-11-27 16:43:09 +0100

    Add test -> ConferencePage + add db for testing

--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -14,4 +14,20 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Give your feedback !');
     }
+
+    public function testConferencePage()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(3, $crawler->filter('h4'));
+
+        $client->clickLink('View');
+
+        $this->assertPageTitleContains('Madrid');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Madrid 2018');
+        $this->assertRouteSame('conference');
+        $this->assertSelectorExists('p:contains("No comment for this conference.")');
+    }
 }
