6ab0fce0aa861c05fd374c5da076e2c13a5fd0a1
Victor <victor.du.77@hotmail.fr>
2021-01-22 13:37:32 +0100

    Fetch data from API

--- spa/src/app.js
+++ spa/src/app.js
@@ -1,12 +1,25 @@
 import {h, render} from 'preact';
 import {Router, Link} from "preact-router";
+import {useState, useEffect} from "preact/hooks";
 
+import {findConferences} from "./api/api";
 import Home from "./pages/home";
 import Conference from "./pages/conference";
 
 import '../assets/css/app.scss'
 
 function App() {
+
+    const [conferences, setConferences] = useState(null);
+
+    useEffect(() => {
+        findConferences().then((conferences) => setConferences(conferences));
+    }, []);
+
+    if (conferences === null) {
+        return <div className="text-center pt-5">Loading...</div>;
+    }
+
     return (
         <div>
             <header className="header">
@@ -19,15 +32,17 @@ function App() {
                 </nav>
 
                 <nav className="bg-light border-bottom text-center">
-                    <Link className="nav-conference" href="/conference/amsterdam2019">
-                        Amsterdam 2019
-                    </Link>
+                    {conferences.map((conference) => (
+                        <Link className="nav-conference" href={'/conference/'+conference.slug}>
+                            {conference.city} {conference.year}
+                        </Link>
+                    ))}
                 </nav>
             </header>
 
             <Router>
-                <Home path="/" />
-                <Conference path="/conference/:slug" />
+                <Home path="/" conferences={conferences} />
+                <Conference path="/conference/:slug" conferences={conferences} />
             </Router>
         </div>
     )
--- spa/webpack.config.js
+++ spa/webpack.config.js
@@ -1,5 +1,6 @@
 const Encore = require('@symfony/webpack-encore');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 Encore
     .setOutputPath('public/')
@@ -10,6 +11,9 @@ Encore
     .enableSassLoader()
     .enableSingleRuntimeChunk()
     .addPlugin(new HtmlWebpackPlugin({ template: 'src/index.ejs', alwaysWriteToDisk: true }))
+    .addPlugin(new webpack.DefinePlugin({
+        'ENV_API_ENDPOINT': JSON.stringify(process.env.API_ENDPOINT),
+    }))
 ;
 
 module.exports = Encore.getWebpackConfig();
